6043f25c60481035383cc5c9eeaf813988772ddf
psalmseen <samsonoyebamiji02@gmail.com>
2022-10-11 12:21:17 +0100

    pwa-todo-app

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/app.ts',
+      entry: 'index.html',
       formats: ['es'],
     },
     rollupOptions: {
