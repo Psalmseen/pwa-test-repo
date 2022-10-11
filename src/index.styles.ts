7876717d6530e7c34a77fd62ccfc311450481420
psalmseen <samsonoyebamiji02@gmail.com>
2022-10-11 08:18:21 +0100

    initial-commit

---
+++ src/index.styles.ts
@@ -0,0 +1,138 @@
+import { css } from 'lit';
+
+export const todoAppStyles = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  .header {
+    text-transform: uppercase;
+    font-size: 2vw;
+    font-family: poppins;
+    width: max-content;
+    background-color: #0eb0e2;
+    padding: 1rem 2.5rem;
+    margin: 0 auto;
+    color: #003748;
+    border-radius: 8px;
+  }
+  .add {
+    font-size: 5vw;
+    font-weight: bolder;
+    height: 5vw;
+    width: 5vw;
+    background-color: #0eb0e2;
+    display: flex;
+    color: #fff;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    cursor: pointer;
+    position: fixed;
+    bottom: 3vw;
+    right: 5vw;
+    transition: all 0.3s;
+  }
+  .close {
+    transform: rotate(45deg);
+  }
+  .modal {
+    position: fixed;
+    inset: calc(100% - 8vw) 5vw 3vw calc(100% - 10vw);
+    transition: all 0.3s;
+    border-radius: 50%;
+  }
+
+  .open-modal {
+    inset: 0;
+    height: unset;
+    width: unset;
+    background-color: #003748;
+    opacity: 0.9;
+    border-radius: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .input-form {
+    border: 1px solid #0eb0e2;
+    border-radius: 0.5rem;
+    display: none;
+    padding: 5rem 3rem;
+    gap: 3rem;
+    flex-direction: column;
+    font-size: 1.4rem;
+  }
+  .input-form * {
+    display: block;
+  }
+  .d-flex {
+    display: flex;
+  }
+  .tasks-container {
+    background-color: #003748;
+    padding: 3rem 4rem 5rem;
+    border-radius: 8px;
+    width: max-content;
+    margin: 3rem auto 1rem;
+    min-width: 50vw;
+
+    max-width: 80vw;
+  }
+  .task-wrapper {
+    border-radius: 3rem 0 0 0;
+    transform: translateY(1rem);
+    padding: 2rem 3rem;
+    box-shadow: 0rem -0.5rem 1rem #0005;
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    gap: 0.7rem;
+    position: relative;
+    font-size: 1.3rem;
+  }
+  .task-wrapper::after {
+    background-color: inherit;
+    content: '';
+    top: 100%;
+    left: 0;
+    right: 0;
+    position: absolute;
+    height: 3rem;
+    border-bottom-right-radius: 3rem;
+  }
+  .task-wrapper:last-of-type {
+    padding-bottom: 2rem;
+    border-bottom-right-radius: 3rem;
+  }
+  .task-wrapper:last-of-type::after {
+    height: 0;
+  }
+  input,
+  textarea {
+    width: 100%;
+    padding: 1rem;
+    margin-top: 0.5rem;
+    outline: none;
+    font-size: 1.2rem;
+    border-radius: 4px;
+    border: none;
+  }
+  textarea {
+    min-height: 7em;
+  }
+  button {
+    padding: 1rem;
+    text-transform: capitalize;
+    font-size: 1.2rem;
+    border: none;
+    border-radius: 4px;
+    outline: none;
+  }
+  button:hover {
+    background-color: #0eb0e2;
+    cursor: pointer;
+  }
+`;
---
+++ src/index.ts
@@ -0,0 +1,98 @@
+import { LitElement, html, CSSResultGroup } from 'lit';
+import { customElement, query, state } from 'lit/decorators.js';
+import { todoAppStyles } from './index.styles';
+import { classMap } from 'lit/directives/class-map.js';
+import { styleMap } from 'lit/directives/style-map.js';
+
+@customElement('todo-app')
+export class TodoApp extends LitElement {
+  static styles: CSSResultGroup = todoAppStyles;
+  @state() isModalOpen = false;
+  @state() tasks = [
+    {
+      id: 1,
+      task: 'Add the date to the todo list',
+      dateOfComplete: '20-10-2022',
+      desc: 'This gives more details about the task to be done',
+    },
+    {
+      id: 2,
+      task: 'Add the date to the todo list',
+      dateOfComplete: '20-10-2022',
+      desc: 'This gives more details about the task to be done',
+    },
+  ];
+
+  @state() colors = [
+    '#D5453C',
+    '#FD9728',
+    '#3D56B0',
+    '#663FB4',
+    '#9F336D',
+    '#EB3B8C',
+  ];
+  @query('#task') task!: HTMLInputElement;
+  @query('#date') date!: HTMLInputElement;
+  @query('#task-desc') taskDesc!: HTMLTextAreaElement;
+  toggleMobileOpen() {
+    this.isModalOpen = !this.isModalOpen;
+  }
+  handleSunmit(e: Event) {
+    e.preventDefault();
+    const newTask = {
+      task: this.task.value,
+      dateOfComplete: this.date.value,
+      desc: this.taskDesc.value,
+      id: this.tasks.length + 1,
+    };
+    this.tasks.push(newTask);
+    this.toggleMobileOpen();
+  }
+
+  protected render(): unknown {
+    return html`<div class="todo-app">
+      <h1 class="header">The todo app</h1>
+      <div class=${classMap({ 'tasks-container': true })}>
+        ${this.tasks.map(
+          ({ id, task, dateOfComplete, desc }) => html` <div
+            class=${classMap({ 'task-wrapper': true })}
+            style=${styleMap({
+              backgroundColor: this.colors[(id - 1) % 6],
+            })}
+            key=${id}
+          >
+            <h2>${task}</h2>
+            <p>${desc}</p>
+            <p>${dateOfComplete}</p>
+          </div>`
+        )}
+      </div>
+      <div class=${classMap({ modal: true, 'open-modal': this.isModalOpen })}>
+        <form
+          class=${classMap({ 'input-form': true, 'd-flex': this.isModalOpen })}
+          @submit=${this.handleSunmit}
+        >
+          <div>
+            <label for="task">Add your task</label>
+            <input id="task" type="text" />
+          </div>
+          <div>
+            <label for="complete-date">Add an expected completion date</label>
+            <input id="date" type="date" id="complete-date" />
+          </div>
+          <div class="task-desc">
+            <label for="task-desc"> Add more details</label>
+            <textarea id="task-desc"></textarea>
+          </div>
+          <button type="submit">Add this task</button>
+        </form>
+      </div>
+      <div
+        @click="${this.toggleMobileOpen}"
+        class=${classMap({ add: true, close: this.isModalOpen })}
+      >
+        +
+      </div>
+    </div>`;
+  }
+}
